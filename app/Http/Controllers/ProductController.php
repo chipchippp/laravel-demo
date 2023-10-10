9075130d5b68bb3d6a66cfd6a4112068936e46df
chipchippp <locnvth2209036@fpt.edu.vn>
2023-10-10 15:54:09 +0700

    admin Crud

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Psy\Util\Str;
+
+class ProductController extends Controller
+{
+    public function index(){
+//        $products = Product::onlyTrashed()->orderBy("id", "desc") -> paginate(20); // lay san pham da xoa
+        $products = Product::orderBy("id", "desc") -> paginate(20);
+        return view('admin.pages.product', ['products' => $products]);
+    }
+    public function product_create(){
+        $categories = Category::all();
+        return view('admin.pages.create', ['categories' => $categories]);
+    }
+    public function store(Request $request){
+        $request->validate([
+           "name"=>"required|min:6" ,
+            "price"=>"required|numeric|min:0",
+            "qty"=>"required|numeric|min:0",
+            "category_id" => "required|numeric|min:1",
+            "thumbnail" => "nullable|mimes:png,jpg,jpeg,gif|mimetypes:image/jpg,image/png,image/jpeg"
+        ]);
+        try {
+            $thumbnail = null;
+            //xu ly upload file
+            if ($request ->hasFile("thumbnail")){
+                $path = public_path("uploads");
+                $file = $request -> file("thumbnail");
+                $file_name = Str::random(5).time().Str::random(5).".".$file->getClientOriginalExtension();
+                $file->move($path, $file_name);
+                $thumbnail = "/uploads".$file_name;
+            }
+            Product::create([
+                'name' =>$request->get("name"),
+                'slug' =>$request->get("slug"),
+                'price' => $request->get("price"),
+                'qty' => $request->get("qty"),
+                'category_id' => $request->get("category_id"),
+                'description' => $request->get("description"),
+            ]);
+             return redirect()->to("/admin/product")->with("success","Successfully");
+        } catch (\Exception $e){
+            return redirect('/admin/product')->back()->withErrors($e->getMessage());
+        }
+    }
+
+    public function edit($id){
+        $product = Product::find($id);
+//        dd($food);
+        return view("admin.pages.edit")->with('product', $product);
+    }
+    public function update(Request $request, Product $product){
+        $request->validate([
+            "name"=>"required|min:6" ,
+            "price"=>"required|numeric|min:0",
+            "qty"=>"required|numeric|min:0",
+            "category_id" => "required|numeric|min:1",
+            "thumbnail" => "nullable|mimes:png,jpg,jpeg,gif|mimetypes:image/jpg,image/png,image/jpeg"
+        ]);
+
+        try {
+            $product->update([
+                    'name' =>$request->get("name"),
+                    'slug' =>$request->get("slug"),
+                    'price' => $request->get("price"),
+                    'qty' => $request->get("qty"),
+                    'category_id' => $request->get("category_id"),
+                    'description' => $request->get("description"),
+                ]);
+            return redirect()->to("/admin/product")->with("success","Successfully");
+
+        }catch (\Exception $e  ){
+            return redirect('/admin/product')->back()->withErrors($e->getMessage());
+        }
+    }
+
+    public function delete(Product $product, $id){
+        try {
+            $product = Product::find($id);
+            $product -> delete();
+            return redirect()->to("/admin/product")->with("success","Successfully");
+
+        }catch (\Exception $e  ){
+            return redirect('/admin/product')->back()->withErrors($e->getMessage());
+        }
+    }
+}
